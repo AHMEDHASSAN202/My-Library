453f1a4dd2c20fe7afc086921bc89492ebcc7570
AHMEDHASSAN202 <ahmedhassanhassan707@gmail.com>
2019-10-27 19:39:39 +0200

    Js

--- Helper.js
+++ Helper.js
@@ -15,3 +15,40 @@ function downloadFile(urlToSend) {
 
   req.send();
 }
+
+/**
+ * Generate SLug Function
+ *
+ * @param Text
+ * @returns {string}
+ */
+function convertToSlug(Text) {
+  return Text.toLowerCase()
+    .replace(/ /g, '-')
+    .replace(/-+/g, '-')
+    .replace(/[`~!@#$%^&*()_|+\=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+}
+
+function ajax(form, beforeSend, success, done) {
+  var data = new FormData($(form)[0]);
+  $.ajax({
+    url: $(form).attr('action'),
+    method: $(form).attr('method'),
+    dataType: $(form).data('dataType') ? $(this).data('dataType') : 'json',
+    data: data,
+    contentType: false,
+    cache: false,
+    processData: false,
+    beforeSend: beforeSend,
+    success: success
+  }).done(done);
+}
+
+function preview() {
+  $('.preview').on('change', function() {
+    preview_target = $(this).data('preview-target');
+    document.getElementById(preview_target).src = window.URL.createObjectURL(
+      this.files[0]
+    );
+  });
+}
